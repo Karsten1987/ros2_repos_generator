7a4293cb9398a39b8e2745de12d2d58f60f8f7db
Karsten Knese <karsten@openrobotics.org>
2019-05-20 10:50:29 -0700

    format and filename argument

--- ros2_repos_generator.py
+++ ros2_repos_generator.py
@@ -14,12 +14,13 @@ import argparse
 import getpass
 import json
 import os
+
 import requests
 from requests.auth import HTTPBasicAuth
 
 api_url = 'https://api.github.com'
 default_ros2_repos = 'https://raw.githubusercontent.com/' \
-        'ros2/ros2/master/ros2.repos'
+    'ros2/ros2/master/ros2.repos'
 
 
 def _get_username_and_password():
@@ -28,20 +29,22 @@ def _get_username_and_password():
     return username, password
 
 
-def _create_gist(repos_content, token_param, auth):
-    gist_file_name = 'external_contribution_repos.txt'
+def _create_gist(repos_content, token_param, auth, file_name):
+    gist_file_name = file_name
     jGist = json.dumps({
-      'description': 'external contribution repos file',
-      'public': True,
-      'files': {
-        gist_file_name: {
-          'content': repos_content}}})
+        'description': 'external contribution repos file',
+        'public': True,
+        'files': {
+            gist_file_name: {
+                'content': repos_content
+            }
+        }})
 
     response = requests.post(
-            api_url + '/gists' + token_param,
-            auth=auth,
-            verify=True,
-            data=jGist)
+        api_url + '/gists' + token_param,
+        auth=auth,
+        verify=True,
+        data=jGist)
     response.raise_for_status()
 
     gist_details = json.loads(response.content.decode())
@@ -56,9 +59,9 @@ def _modify_master_repos(repos, pkg, url, branch):
         if pkg + ':' == val.lstrip():
             pkg_found = True
             lines[idx + 2] = '{indent}url: {url}'.format(
-                    indent=4 * ' ', url=url)
+                indent=4 * ' ', url=url)
             lines[idx + 3] = '{indent}version: {branch}'.format(
-                    indent=4 * ' ', branch=branch)
+                indent=4 * ' ', branch=branch)
             print('new entry for package:', val.lstrip())
             [print(lines[idx + i] for i in range(4))]
             print(lines[idx + 1])
@@ -69,13 +72,13 @@ def _modify_master_repos(repos, pkg, url, branch):
     if not pkg_found:
         new_entry = []
         new_entry.append('{indent}{pkg}:'.format(
-                indent=2 * ' ', pkg=pkg))
+            indent=2 * ' ', pkg=pkg))
         new_entry.append('{indent}type: git'.format(
-                indent=4 * ' '))
+            indent=4 * ' '))
         new_entry.append('{indent}url: {url}'.format(
-                indent=4 * ' ', url=url))
+            indent=4 * ' ', url=url))
         new_entry.append('{indent}version: {branch}'.format(
-                indent=4 * ' ', branch=branch))
+            indent=4 * ' ', branch=branch))
         print('adding new package entry:')
         [print(new_entry[x]) for x in range(len(new_entry))]
         del lines[-1]  # remove last '\n'
@@ -96,21 +99,21 @@ def _fetch_pr_info(pr_url):
     pr_id = url_items[6]
     if org is None:
         raise IndexError(
-                'could not find any github organization in url {url}'.format(
-                    url=pr_url))
+            'could not find any github organization in url {url}'.format(
+                url=pr_url))
     if repo is None:
         raise IndexError(
-                'could not find any github repository in url {url}'.format(
-                    url=pr_url))
+            'could not find any github repository in url {url}'.format(
+                url=pr_url))
     if pr_id is None or not pr_id.isdigit():
         raise IndexError(
-                'could not find any pull request id in url {url}'.format(
-                    url=pr_url))
+            'could not find any pull request id in url {url}'.format(
+                url=pr_url))
 
     print('analyzing pull request url', pr_url)
 
     request_url = api_url + '/repos/{org}/{repo}/pulls/{pr_id}'.format(
-            org=org, repo=repo, pr_id=pr_id)
+        org=org, repo=repo, pr_id=pr_id)
     pr_info = requests.get(request_url)
     jPr = json.loads(pr_info.content.decode())
 
@@ -124,13 +127,17 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            'pr_url',
-            nargs='+',
-            help='modify repos file with these PR url')
+        'pr_url',
+        nargs='+',
+        help='modify repos file with these PR url')
     parser.add_argument(
-            '-master_repos_url',
-            nargs='?',
-            help='original repos file where to merge PRs into')
+        '-m', '--master_repos_url',
+        nargs='?',
+        help='original repos file where to merge PRs into')
+    parser.add_argument(
+        '-f', '--gist-file-name',
+        type=str, default='external_contribution.txt',
+        help='file name of the gist to be created')
     args = parser.parse_args()
 
     auth = None
@@ -150,7 +157,7 @@ if __name__ == '__main__':
     for pr in args.pr_url:
         pkg, url, branch = _fetch_pr_info(pr)
         ros2_repos = _modify_master_repos(ros2_repos, pkg, url, branch)
-    gist_url = _create_gist(ros2_repos, token_param, auth)
+    gist_url = _create_gist(ros2_repos, token_param, auth, args.gist_file_name)
 
     print('new gist url:')
     print(gist_url)
